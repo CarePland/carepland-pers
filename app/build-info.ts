27b926e4a07bcc4c6aecc4768246b643d029097c
CarePland <support@carepland.com>
2026-07-22 02:03:30 -0700

    chore: refresh build metadata

--- app/build-info.ts
+++ app/build-info.ts
@@ -1,2 +1,2 @@
-export const generatedBuildDttm = "7/22/26, 2:02 AM";
-export const generatedBuildNumber = "f30b0e66";
+export const generatedBuildDttm = "7/22/26, 2:03 AM";
+export const generatedBuildNumber = "f9d4fb69";
